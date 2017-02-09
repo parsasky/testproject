443f66618046124753980aece955a3b3b22a21ee
parsasky <parsasky@gmail.com>
2017-02-09 21:32:47 +0330

    delete echo

--- index.php
+++ index.php
@@ -6,6 +6,15 @@ QRcode::png('457584842','1.png', QR_ECLEVEL_H, 60, 1);
 QRcode::png('45758484','o.png');
 echo '<img src="'.QRcode::png('45758484').'" />';
 
-echo '<img src="'.QRcode::png('000000000').'" />';
+
+
+
+
+
+
+
+
+
+
 
 ?>
