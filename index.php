df10c4896968b39a1d8aa16673d4d1b01670bd9c
parsasky <parsasky@gmail.com>
2017-02-09 21:41:00 +0330

    pppp

--- index.php
+++ index.php
@@ -5,16 +5,4 @@ include('qr/qrlib.php');
 QRcode::png('457584842','1.png', QR_ECLEVEL_H, 60, 1);
 QRcode::png('45758484','o.png');
 echo '<img src="'.QRcode::png('45758484').'" />';
-
-
-
-
-
-
-
-
-
-
-
-
 ?>
