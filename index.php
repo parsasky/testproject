ca5e904a690f2f2d432f21e141ea64d18e207510
parsasky <parsasky@users.noreply.github.com>
2017-02-09 19:00:16 +0330

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+include('qr/qrlib.php'); 
+
+QRcode::png('457584842','1.png', QR_ECLEVEL_H, 60, 1);
+QRcode::png('45758484','o.png');
+echo '<img src="'.QRcode::png('45758484').'" />';
+
+
+
+
+?>
