9148bdb3a09359358684c9668039bb1b105afe93
parsasky <parsasky@gmail.com>
2017-02-09 21:44:04 +0330

    9999999

--- index.php
+++ index.php
@@ -4,5 +4,8 @@ include('qr/qrlib.php');
 
 QRcode::png('457584842','1.png', QR_ECLEVEL_H, 60, 1);
 QRcode::png('45758484','o.png');
-echo '<img src="'.QRcode::png('45758484').'" />';
+echo '<img src="'.QRcode::png('000').'" />';
+
+
+
 ?>
